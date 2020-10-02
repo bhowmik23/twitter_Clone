c558106d15fab744ab528cf4175d5b0c984e58e5
Biddut Bhowmik <54990169+bhowmik23@users.noreply.github.com>
2020-10-02 23:43:59 +0600

    File storage and costom avatars done

--- resources/views/profiles/edit.blade.php
+++ resources/views/profiles/edit.blade.php
@@ -49,23 +49,28 @@
       </div>
 
       <div class="mb-6">
+         
          <label class="block mb-2 uppercase font-bold text-xs text-gray-700"
             for="avatar"
          >
             Avatar
          </label>
-         <div class="col-md-6">
+         <div class="flex">
             <input id="avatar" 
                type="file" 
                class="border border-gray-400 p-2 w-full"
                name="avatar" 
                required
-             >
-
-            @error('avatar')
-                  <p class="text-red-500 text-xs mt-2">{{ $message }}</p>
-            @enderror
+            >
+            <img src="{{ $user->avatar }}"
+               alt="your avatar"
+               width="40" 
+            >
          </div>
+         @error('avatar')
+            <p class="text-red-500 text-xs mt-2">{{ $message }}</p>
+         @enderror
+        
       </div>
 
       <div class="md-6">

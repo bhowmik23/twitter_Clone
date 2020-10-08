ace244f693cf2cc0870fd5fbe28dbf585c4747cf
Biddut Bhowmik <54990169+bhowmik23@users.noreply.github.com>
2020-10-08 23:21:51 +0600

    likes dislike partially done

--- app/Models/Tweet.php
+++ app/Models/Tweet.php
@@ -9,14 +9,46 @@ class Tweet extends Model
 {
     use HasFactory;
 
-    // protected $fillable= [
-    //     'user_id', 'body'
-    // ];
-
     protected $guarded  = [];
 
     public function user()
     {
         return $this->belongsTo(User::class);
+    }
+     
+    public function like($user = null, $liked = true)
+    {
+        return $this->likes()->updateOrCreate([
+            'user_id' => $user ? $user->id : auth()->id(),
+        ],
+        [
+            'liked' => $liked,
+        ]);
+    }
+
+    public function dislike($user  = null)
+    {
+        return $this->like($user, false);
+    }
+
+    public function isLikedBy(User $user)
+    {
+        return (bool) $user->likes()
+            ->where('tweet_id', $this->id)
+            ->where('liked', true)
+            ->count();
+    }
+
+    public function isDislikedBy(User $user)
+    {
+        return (bool) $user->likes()
+            ->where('tweet_id', $this->id)
+            ->where('liked', false)
+            ->count();
+    }
+
+    public function likes()
+    {
+        return $this->hasMany(Like::class);
     }
 }
